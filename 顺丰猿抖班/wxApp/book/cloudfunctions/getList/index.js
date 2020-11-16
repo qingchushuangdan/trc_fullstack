69092d8eef5882a123a295c7b2c4ab6ff5dafb64
qingchushuangdan <549715179@qq.com>
2020-11-16 17:25:24 +0800

    小说云开发

---
+++ 顺丰猿抖班/wxApp/book/cloudfunctions/getList/index.js
@@ -0,0 +1,30 @@
+// 云函数入口文件
+const cloud = require('wx-server-sdk')
+const rp = require('request-promise')// http模块
+const cheerio = require('cheerio')
+let charset = require('superagent-charset')//解决乱码
+let superagent = require('superagent')//发起请求
+charset(superagent)
+cloud.init()
+
+// 云函数入口函数
+exports.main = async (event, context) => {
+  let serverUrl = "https://wap.biqiuge8.com/"
+  const result = await superagent.get(serverUrl).charset('gb2312') //取决于网页的编码方式
+  const data = result.text || ''
+  const $ = cheerio.load(result.text)
+  let hotList = $('.hot').find('.image')
+  let hotData = []
+  for (let i = 0; i < hotList.length; i++){
+    let obj = {}
+    obj['url'] = $(hotList[i]).find('a').attr('href')
+    obj['imgurl'] = $(hotList[i]).find('img').attr('src')
+    obj['name'] = $(hotList[i]).find('img').attr('alt')
+    obj['author'] = $(hotList[i]).next().find('dt').find('span').text()
+    obj['detail'] = $(hotList[i]).next().find('dd').text()
+    hotData.push(obj)
+  }
+  return {
+    hotData
+  }
+}
---
+++ 顺丰猿抖班/wxApp/book/miniprogram/pages/index/index.js
@@ -0,0 +1,79 @@
+// miniprogram/pages/index/index.js
+const app = getApp()
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    avatarUrl: './user-unlogin.png',
+    userInfo: {},
+    hasUserInfo: false
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+
+  },
+  onGetUserInfo(e){
+    // console.log(e);
+    if (e.detail.userInfo) {
+      app.globalData.userInfo = e.detail.userInfo
+      this.setData({
+        avatarUrl: e.detail.userInfo.avatarUrl,
+        userInfo: e.detail.userInfo,
+        hasUserInfo: true
+      })
+    }
+  },
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  }
+})
