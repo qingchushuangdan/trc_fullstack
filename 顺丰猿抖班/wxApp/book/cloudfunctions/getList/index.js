8ef18c49aad1e047ecac1ddf4de1807c9b49bee9
qingchushuangdan <549715179@qq.com>
2020-11-17 19:16:29 +0800

    小说小程序

--- 顺丰猿抖班/wxApp/book/cloudfunctions/getList/index.js
+++ 顺丰猿抖班/wxApp/book/cloudfunctions/getList/index.js
@@ -12,7 +12,9 @@ exports.main = async (event, context) => {
   let serverUrl = "https://wap.biqiuge8.com/"
   const result = await superagent.get(serverUrl).charset('gb2312') //取决于网页的编码方式
   const data = result.text || ''
-  const $ = cheerio.load(result.text)
+  const $ = cheerio.load(data)
+
+  // 热门推荐
   let hotList = $('.hot').find('.image')
   let hotData = []
   for (let i = 0; i < hotList.length; i++){
@@ -24,7 +26,28 @@ exports.main = async (event, context) => {
     obj['detail'] = $(hotList[i]).next().find('dd').text()
     hotData.push(obj)
   }
+
+  // 分类推荐
+  let classifyList = $('.block')
+  let classifyData = []
+  for (let i = 0; i < classifyList.length; i++) {
+    let obj = {}
+    let childData = []
+    let childDom = $(classifyList[i]).find('.lis').find('li')
+    for (let j = 0; j < childDom.length; j++) {
+      let childObj = {}
+      childObj['name'] = $(childDom[j]).find('.s2').find('a').text()
+      childObj['name'] = $(childDom[j]).find('.s2').find('a').text()
+      childObj['author'] = $(childDom[j]).find('.s3').text()
+      childData.push(childObj)
+    }
+    obj['classifyList'] = $(classifyList[i]).find('h2').text()
+    obj['data'] = childData
+    classifyData.push(obj)
+
+  }
   return {
-    hotData
+    hotData,
+    classifyData
   }
 }
--- 顺丰猿抖班/wxApp/book/miniprogram/pages/bookSection/bookSection.js
+++ 顺丰猿抖班/wxApp/book/miniprogram/pages/bookSection/bookSection.js
@@ -1,42 +1,37 @@
-// miniprogram/pages/bookCity/bookCity.js
+// miniprogram/pages/bookSection/bookSection.js
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-    hotData: [],
-    classifyData: []
+    bookDetailData:{}
   },
-  getList() {
+  getSection(url) {
     wx.showLoading({
       title: '正在加载'
     })
     wx.cloud.callFunction({
-      name: 'getList',
-      data: {}
+      name: 'bookSection',
+      data: {
+        url: url
+      }
     }).then(res => {
-      // console.log(res);
+      console.log(res);
       wx.hideLoading()
-      const result = res.result || {}
+      const {result} = res
       this.setData({
-        hotData: result.hotData,
-        classifyData: result.classifyData
+        bookDetailData: result.bookDetailData
       })
-      console.log(this.data.classifyData)
-    })
-  },
-  toReading(e) {
-    let url = e.currentTarget.dataset.url
-    wx.navigateTo({
-      url: `../bookSection/bookSection?url=${url}`
     })
   },
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-    this.getList()
+    console.log(options);
+    const {url} = options
+    this.getSection(url)
   },
 
   /**
