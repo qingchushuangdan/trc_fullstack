1d8aa7ca57ee71f79da0e732288db46ba1d1698a
qingchushuangdan <549715179@qq.com>
2020-11-19 22:59:56 +0800

    手写系列-防抖

---
+++ 顺丰猿抖班/JS/手写系列/防抖/index.js
@@ -0,0 +1,13 @@
+// 在一定的时间内，如果持续请求，就一直不执行，在规定的时间没有第二次操作才执行
+function debounce(fn, delay) {
+  let timer = null
+  return function () {
+    let context = this
+    let arg = arguments
+    clearTimeout(timer)
+    timer = setTimeout(() => {
+      fn.apply(context, arg)
+    }, delay)
+  }
+
+}
