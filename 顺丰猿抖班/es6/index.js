6ee6b2c5cdbb73ba2ea97ea8c34275aac6fac445
qingchushuangdan <549715179@qq.com>
2020-11-21 12:05:58 +0800

    index

---
+++ 顺丰猿抖班/es6/index.js
@@ -0,0 +1,13 @@
+// var foo = 'bar'
+// console.log(foo);
+// foo = 'bar'
+
+// const foo = 'bar'
+// foo = 123  // 此行报错
+// console.log(foo);
+
+// let example = 'tom'
+// const foo = 'this is' + example
+// const foo = `this is ${example}`  // es6模板拼接
+
+
