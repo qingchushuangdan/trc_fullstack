8237a6beb30bb9302d7c2b4fae0aca193202da6a
qingchushuangdan <549715179@qq.com>
2020-11-30 18:16:02 +0800

    vue-elm

---
+++ 顺丰猿抖班/vue/vue-elm/src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <div id="app">
+    <Header></Header>
+  </div>
+</template>
+
+<script>
+import Header from '@/components/header/Header'
+export default {
+  components: {
+    Header
+  }
+}
+</script>
+
+<style>
+
+</style>
---
+++ 顺丰猿抖班/vue/vue-elm/src/components/header/Header.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="header">
+    <div class="content-wrapper">
+      <div class="avatar">
+        <img
+          width="64"
+          height="64"
+          src="https://ss2.bdstatic.com/70cFvnSh_Q1YnxGkpoWK1HF6hhy/it/u=2944606985,3722907313&fm=11&gp=0.jpg"
+          alt=""
+        />
+      </div>
+      <div class="content">
+        <div class="title">
+          <span class="brand">品牌</span>
+          <span class="name">粥品香坊（回龙观）</span>
+        </div>
+        <div class="description">蜂鸟专送/38分钟送达</div>
+        <div class="support">
+          <img src="./decrease_1@2x.png" alt="">
+          <span class="text">在线支付满28减5</span>
+        </div>
+      </div>
+    </div>
+    <div class="bulletin-wrapper"></div>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+</style>
