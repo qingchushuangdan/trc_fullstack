eadd1cb3908495c02b665585a66e69e8db642e57
qingchushuangdan <549715179@qq.com>
2020-12-29 20:23:53 +0800

    event-bus

---
+++ 顺丰猿抖班/JS/手写系列/Event-Bus/index.js
@@ -0,0 +1,25 @@
+class EventEmitter {
+  constructor () {
+    // 存储事件
+    this.events = this.events || new Map()
+  }
+  // 监听事件
+  addListener (type, fn) {
+    if (!this.events.get(type)) {
+      this.events.set(type, fn)
+    }
+  }
+  // 触发事件
+  emit (type) {
+    let handle = this.events.get(type)
+    handle.apply(this, [...arguments].slice(1))
+  }
+}
+
+let emitter = new EventEmitter()
+emitter.addListener('ages', age => {
+  console.log(age);
+})
+emitter.emit('ages', 18)
+
+// this.$emit('handle')
