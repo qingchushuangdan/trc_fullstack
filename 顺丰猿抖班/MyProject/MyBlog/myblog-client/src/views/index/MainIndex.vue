a73df2bbda8495e5b1d7c8d758b6c39e2831820e
qingchushuangdan <549715179@qq.com>
2021-02-07 17:20:47 +0800

    myproject

---
+++ 顺丰猿抖班/MyProject/MyBlog/myblog-client/src/views/index/MainIndex.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="index">
+    <div class="header">
+      
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
---
+++ 顺丰猿抖班/MyProject/MyBlog/myblog-client/src/views/login/Login.vue
@@ -0,0 +1,49 @@
+<template>
+  <div class="star-login">
+    <h1>登录</h1>
+    <div class="login-wrapper">
+      <div class="avatar" :style="`background-image: url(${avatar})`"></div>
+      <div class="input-group">
+        <label for="username">账号</label>
+        <input type="text" id="username" v-model="username">
+      </div>
+      <div class="input-group input-group-panel">
+        <label for="userpwd">密码</label>
+        <input type="password" id="userpwd" v-model="userpwd">
+      </div>
+      <p class="forgot-pwd">忘记密码</p>
+      <div class="sign">登录</div>
+    </div>
+    <div class="register">新用户？点击这里注册吧！</div>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      avatar: require('./../../assets/img/raw_1512446140.jpeg')
+    }
+  }
+}
+</script>
+
+<style lang="less" scoped>
+input {
+  border: 0;
+  outline: none;
+}
+.star-login {
+  width: 100%;
+  height: 100vh;
+  background-color: #fff;
+  box-sizing: border-box;
+  padding: 0 1.28rem;
+  overflow: hidden;
+  h1 {
+    margin-top: 1.12rem;
+    height: 0.6933rem;
+    line-height: 0.6933rem;
+  }
+}
+</style>
