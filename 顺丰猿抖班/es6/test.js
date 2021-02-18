9fc43bd2d2930a1365283fc2614c813ef65d78ba
qingchushuangdan <549715179@qq.com>
2021-02-18 23:05:55 +0800

    test

--- 顺丰猿抖班/es6/test.js
+++ 顺丰猿抖班/es6/test.js
@@ -2,7 +2,7 @@ var sumOddLengthSubarrays = function(arr) {
   let result = 0
   for ( let i = 1; i <= arr.length; i += 2) { // i = 1 i = 3
     for (let j = 0; j < arr.length; j++) { // j = 0 j = 0
-      if (arr[j + i -1] != null) {
+     if (arr[j + i -1] != null) {
         for (let k = 0; k < i; k++) { // k = 0
           result = result + arr[j + k]
         }
@@ -14,4 +14,4 @@ var sumOddLengthSubarrays = function(arr) {
 
 console.log(sumOddLengthSubarrays([1, 4, 2, 5, 3]));
 
-///////////////////////
+//////////////////////////////
